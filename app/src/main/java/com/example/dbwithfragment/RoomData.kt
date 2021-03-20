829e37cc2da6fb5127b67cb694edd45399bde5af
toy9910 <toy9910@naver.com>
2021-03-20 15:24:08 +0900

    initial commit

---
+++ app/src/main/java/com/example/dbwithfragment/RoomData.kt
@@ -0,0 +1,9 @@
+package com.example.dbwithfragment
+
+class RoomData {
+    lateinit var room_no : String
+    lateinit var room_temperature : String
+    lateinit var room_humidity : String
+    lateinit var room_gas : String
+    lateinit var room_light : String
+}
